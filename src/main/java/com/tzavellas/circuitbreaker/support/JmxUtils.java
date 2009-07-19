bcd449e54d18eb23a6e81bf9528a2a5764d172c5
Spiros Tzavellas <spiros@tzavellas.com>
2009-07-19 20:54:19 +0300

    Removed broken javadocs.

--- src/main/java/com/tzavellas/circuitbreaker/support/JmxUtils.java
+++ src/main/java/com/tzavellas/circuitbreaker/support/JmxUtils.java
@@ -42,16 +42,6 @@ public abstract class JmxUtils {
 		return mbeans;
 	}
 
-
-	/**
-	 * Get the ObjectName (as a String) used to register the ...
-	 * 
-	 * The format of the {@code ObjectName} used for each {@code CircuitInfoMBean} is:
-	 * {@literal com.tzavellas.circuitbreaker:type=CircuitInfo,target=targetName},
-	 * where targetName is the simple name of the class of the target (defined
-	 * by the circuit() pointcut). 
-	 * 
-	 */
 	public static String getObjectName(Object target) {
 		return String.format("com.tzavellas.circuitbreaker:type=CircuitInfo,target=%s,code=%d",
 				target.getClass().getSimpleName(),
